589ff1ce9f2bf24d791567af3072a70b6bf8fc24
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-09-30 18:05:34 +0100

    Initial Commit

---
+++ Coinvert/CoinvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinvertApp.swift
+//  Coinvert
+//
+//  Created by Evan Plant on 30/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoinvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
