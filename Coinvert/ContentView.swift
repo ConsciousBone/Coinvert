a09fc4f69694162b1fc25e513501e3fb62a2b256
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-10-04 13:11:14 +0100

    add accent colour functionality

--- Coinvert/ContentView.swift
+++ Coinvert/ContentView.swift
@@ -8,6 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("selectedAccentIndex") private var selectedAccentIndex = 3 // Color.green
+    let accentColours = [ // i robbed this from Searchino, i cba doing accent shit again
+        Color.red.gradient,     Color.orange.gradient,
+        Color.yellow.gradient,  Color.green.gradient,
+        Color.mint.gradient,    Color.blue.gradient,
+        Color.purple.gradient,  Color.brown.gradient,
+        Color.white.gradient,   Color.black.gradient
+    ]
+    
     var body: some View {
         TabView {
             Tab("Conversion", systemImage: "shuffle") {
@@ -20,7 +29,7 @@ struct ContentView: View {
                 SettingsView()
             }
         }
-        .tint(Color(.green))
+        .tint(accentColours[selectedAccentIndex])
     }
 }
 
--- Coinvert/Views/SettingsView.swift
+++ Coinvert/Views/SettingsView.swift
@@ -12,7 +12,21 @@ struct SettingsView: View {
     @AppStorage("defaultBaseCurrency") private var defaultBaseCurrency = ""
     @AppStorage("defaultBaseCurrency") private var defaultWantedCurrency = ""
     
-    @AppStorage("selectedAccentIndex") private var selectedAccentIndex = 4
+    @AppStorage("selectedAccentIndex") private var selectedAccentIndex = 3 // Color.green
+    let accentColours = [ // i robbed this from Searchino, i cba doing accent shit again
+        Color.red.gradient,     Color.orange.gradient,
+        Color.yellow.gradient,  Color.green.gradient,
+        Color.mint.gradient,    Color.blue.gradient,
+        Color.purple.gradient,  Color.brown.gradient,
+        Color.white.gradient,   Color.black.gradient
+    ]
+    let accentColourNames = [ // again, robbed from Searchino, dw i own Searchino haha its not been in a YSWS either
+        String(localized: "Red"),     String(localized: "Orange"),
+        String(localized: "Yellow"),  String(localized: "Green"),
+        String(localized: "Mint"),    String(localized: "Blue"),
+        String(localized: "Purple"),  String(localized: "Brown"),
+        String(localized: "White"),   String(localized: "Black")
+    ]
     
     var body: some View {
         NavigationStack {
@@ -32,7 +46,17 @@ struct SettingsView: View {
                 }
                 
                 Section { // accent
-                    // TODO: accent colour shit
+                    Picker(selection: $selectedAccentIndex, label: Label("Accent Colour", systemImage: "paintpalette")) {
+                        ForEach(accentColours.indices, id: \.self) { index in
+                            HStack {
+                                Circle()
+                                    .fill(accentColours[index])
+                                    .frame(width: 15)
+                                Text(accentColourNames[index])
+                            }
+                            .tag(index)
+                        }
+                    }
                 }
             }
             .navigationTitle("Settings")
